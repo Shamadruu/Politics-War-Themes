f80394d62d48bbe25d56eb2b8848e409e5e8e1dd
Shamadruu <jacksonayres11@gmail.com>
2020-08-21 19:57:43 -0600

    Add files via upload

--- dark.user.js
+++ dark.user.js
@@ -1,1 +1,36 @@
-	
+// ==UserScript==
+// @name         Dark Theme for Poliitics and War
+// @namespace    https://github.com/Shamadruu/thegrid/raw/master/
+// @version      1.0
+// @description  try to take over the world!
+// @author       Shamadruu
+// @downloadURL  https://raw.githubusercontent.com/Shamadruu/thegrid/master/shamui.user.js
+// @updateURL    https://raw.githubusercontent.com/Shamadruu/thegrid/master/shamui.user.js
+// @match        https://politicsandwar.com/*
+// @grant        none
+// ==/UserScript==
+(function(){
+	var addGlobalStyle = function(style) {
+        let styleElement = document.createElement("style");
+        styleElement.innerHTML = style;
+        document.head.appendChild(styleElement);
+    }
+	addGlobalStyle('body > * {background-color: #272b30!important;}');
+	addGlobalStyle('div.hidden-xs:nth-child(1) { background-color: #3c3c3c;}');
+	addGlobalStyle('body a {color: #33ccff;}');
+	addGlobalStyle('body a:hover {color: #cc33ff;}');
+	addGlobalStyle('.informationbar, .columnheader, body table.nationtable th {background-color: #003d99');
+	addGlobalStyle('body p, #leftcolumn,body table.nationtable { color:#c8c8c8;}');
+	addGlobalStyle('.achievement-showcase-title {color: #d7d77c');
+	addGlobalStyle('#header > img:nth-child(1) {filter: invert(1);}');
+	addGlobalStyle('div.col-md-1:nth-child(1) > img:nth-child(1), div.col-md-1:nth-child(3) > img:nth-child(1) {filter:invert(1);}');
+	addGlobalStyle('div.btn-group > .btn {background-color:#ccc; border-color: #3b4145; }');
+	addGlobalStyle('.nationtable tr:nth-child(2n+1) {background-color:#3b4145;}');
+	addGlobalStyle('.nationtable tr:nth-child(2n) {background-color:#272b30;}');
+	addGlobalStyle('.nationtable tr, table.nationtable td {border: #595959 solid 1px;}');
+	addGlobalStyle('.nation-activity {background-color: #595959;}');
+	addGlobalStyle('.plot-container .svg-container {filter:invert(.85);}');
+	addGlobalStyle('.breadcrumb {background-color: #454545;}');
+	addGlobalStyle('.breadcrumb li {color:#ccc!important;}');
+	addGlobalStyle('.alert.alert-warning.center, .alert.alert-danger {background-color: #3c3c3c;}');
+})();
