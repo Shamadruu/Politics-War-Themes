658c33b07dbf2e838e064969bd518c8ee5ef6725
Shamadruu <jacksonayres11@gmail.com>
2021-05-03 13:03:03 -0600

    Update dark.user.js

--- dark.user.js
+++ dark.user.js
@@ -41,4 +41,5 @@
 	addGlobalStyle('.blue-msg {background: #4084bf;}');
     addGlobalStyle('.ck.ck-editor__main * {color: black;}')
     addGlobalStyle('#rightcolumn {color: #c8c8c8!important}');
+    addGlobalStyle('.alert-success {background-color: #90b181!important}');
 })();
