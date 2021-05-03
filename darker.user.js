8e55b9621b57c68b47fb7e2cd085ce90b11bc210
Shamadruu <jacksonayres11@gmail.com>
2021-05-03 13:19:39 -0600

    Update darker.user.js

--- darker.user.js
+++ darker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Darker Theme for Poliitics and War
 // @namespace    https://github.com/Shamadruu/Politics-War-Themes/raw/master/
-// @version      2.1
+// @version      2.2
 // @description  try to take over the world!
 // @author       Shamadruu
 // @downloadURL  https://github.com/Shamadruu/Politics-War-Themes/raw/master/darker.user.js
