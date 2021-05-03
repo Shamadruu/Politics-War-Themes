ee8e3fa204ab849e7510e881cc8bca674a7ff92e
Shamadruu <jacksonayres11@gmail.com>
2021-05-03 12:42:50 -0600

    Update dark.user.js

--- dark.user.js
+++ dark.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dark Theme for Poliitics and War
 // @namespace    https://github.com/Shamadruu/Politics-War-Themes/raw/master/
-// @version      1.9
+// @version      2.0
 // @description  try to take over the world!
 // @author       Shamadruu
 // @downloadURL  https://github.com/Shamadruu/Politics-War-Themes/raw/master/dark.user.js
